2237c9a679bc211dd6144d4568ecb78354535330
vaycomolokko <ultimahmet@hotmail.com>
2024-05-18 16:45:20 +0300

    update packages

--- client/src/features/auth/components/RequireAuth.jsx
+++ client/src/features/auth/components/RequireAuth.jsx
@@ -3,9 +3,9 @@ import useAuth from "../../../hook/useAuth";
 
 const RequireAuth = () => {
   const { auth } = useAuth();
-  return auth.accessToken != null &&
-    auth.accessToken != undefined &&
-    auth.accessToken != "" ? (
+  return auth.accessToken !== null &&
+    auth.accessToken !== undefined &&
+    auth.accessToken !== "" ? (
     <Outlet />
   ) : (
     <Navigate to="/login" />
